5716a793bf15921dc251df92ed67f2d0f7482f09
kenchiu21 <kenchiu21@gmail.com>
2020-02-17 23:59:44 +0800

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input String; ");
+
+        String inputStr = bufferedReader.readLine();
+        StringBuilder sb = new StringBuilder();
+        Iterator<String> it = Arrays.asList(inputStr.split("")).iterator();
+        Map<String, Integer> charMap = new HashMap<>();
+
+        String prevChar = null;
+        while (it.hasNext()) {
+            String current = it.next();
+            Integer count = charMap.get(current) == null ? 1 : charMap.get(current);
+
+            if(prevChar != null && !prevChar.equals(current)) {
+                sb.append(prevChar);
+                charMap.remove(prevChar);
+            }
+
+            prevChar = current;
+            charMap.put(current, count);
+
+            if(!it.hasNext() && charMap.get(current) != null) {
+                sb.append(current);
+            }
+        }
+
+
+
+        System.out.println(sb.toString());
+    }
+
+}
